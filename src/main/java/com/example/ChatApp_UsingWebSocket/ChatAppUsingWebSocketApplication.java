c749154ad4d6a71a83bec5c769486fe0f758cebd
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-05-14 10:59:08 +0530

    Initial commit

---
+++ src/main/java/com/example/ChatApp_UsingWebSocket/ChatAppUsingWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.ChatApp_UsingWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppUsingWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppUsingWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ChatApp_UsingWebSocket/ChatAppUsingWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChatApp_UsingWebSocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppUsingWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

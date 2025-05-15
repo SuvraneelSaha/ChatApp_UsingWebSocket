080b51264c077e8299b82ddc51d8545966ad2092
suvroneel2001@gmail.com <suvroneel2001@gmail.com>
2025-05-15 11:05:17 +0530

    done till window closing and a popup dialog box

--- src/main/java/com/example/ChatApp_UsingWebSocket/client/ClientGUI.java
+++ src/main/java/com/example/ChatApp_UsingWebSocket/client/ClientGUI.java
@@ -3,6 +3,8 @@ package com.example.ChatApp_UsingWebSocket.client;
 import com.example.ChatApp_UsingWebSocket.Message;
 
 import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.concurrent.ExecutionException;
 
 public class ClientGUI extends JFrame {
@@ -20,7 +22,50 @@ public class ClientGUI extends JFrame {
         setLocationRelativeTo(null);
 
         // By Default the close operation of JFrame is to hide the application and run it in the background
-        // and we need to change that . 
+        // and we need to change that .
+
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int option = JOptionPane.showConfirmDialog(ClientGUI.this,"Do you really want to Leave ?", "Exit" , JOptionPane.YES_NO_OPTION );
+
+                if (option == JOptionPane.YES_OPTION){
+                    ClientGUI.this.dispose();
+
+                }
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+        });
 
     }
 }
